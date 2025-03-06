6b8c73034de994fc899e72c36309dcbc4dcbb1c9
saibhargavi-16 <saibhargavimittapellywar@gmail.com>
2025-03-06 20:31:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (86.95%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        //int x=0;
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2!=0){
+                ans++;
+                if(ans==3) return true;
+            } 
+            else ans=0;
+        }
+        return false;
+    }
+}
