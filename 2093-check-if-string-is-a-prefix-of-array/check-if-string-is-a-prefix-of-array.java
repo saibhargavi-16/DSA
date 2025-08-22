0059d3d0336ce4420791811b592c929d00b46e9d
saibhargavi-16 <saibhargavimittapellywar@gmail.com>
2025-08-22 14:33:18 +0530

    Time: 1 ms (78.79%) | Memory: 42.6 MB (61.31%) - LeetSync

---
+++ 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPrefixString(String s, String[] words) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<words.length;i++){
+            sb.append(words[i]);
+            if(sb.length()>s.length()) return false;
+            if(sb.toString().equals(s)) return true;
+        }
+        return false;
+    }
+}
