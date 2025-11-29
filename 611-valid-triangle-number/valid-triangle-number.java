7a4f8c55d1f8dbbeacb06ca2365121eccaab401f
saibhargavi-16 <saibhargavimittapellywar@gmail.com>
2025-11-29 15:54:45 +0530

    Time: 27 ms (50.28%) | Memory: 45.4 MB (25.50%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int c=0;
+        for(int i=nums.length-1;i>=2;i--){
+            int l=0,r=i-1;
+            while(l<r){
+                if(nums[l]+nums[r]>nums[i]){
+                    c+=r-l;
+                    r--;
+                }
+                else l++;
+            }
+        }
+        return c;
+    }
+}
