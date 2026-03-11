a561ccc192be41fc58ea0e84438384873ea1a58e
saibhargavi-16 <saibhargavimittapellywar@gmail.com>
2026-03-11 16:51:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (96.33%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,6 +1,11 @@
 class Solution {
     public int fib(int n) {
-        if(n==0 || n==1) return n;
-        return fib(n-1)+fib(n-2);
+        int a=0,b=1,c=0;
+        for(int i=0;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return a;
     }
 }
